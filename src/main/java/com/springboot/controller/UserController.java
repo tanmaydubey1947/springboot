adccba836c9d96224f0bcff151bb94f4638f70e3
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-06-22 17:38:43 +0530

    Logging properties added

--- src/main/java/com/springboot/controller/UserController.java
+++ src/main/java/com/springboot/controller/UserController.java
@@ -5,6 +5,8 @@ import com.springboot.dto.UserRequestDTO;
 import com.springboot.dto.UserResponseDTO;
 import com.springboot.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/{userId}")
     public ServiceResponse<UserResponseDTO> getById(@PathVariable int userId) {
         UserResponseDTO userResponseDTO = userService.getById(userId);
@@ -26,5 +30,15 @@ public class UserController {
     public ServiceResponse<UserResponseDTO> addUser(@RequestBody @Valid UserRequestDTO userRequestDTO) {
         UserResponseDTO userResponseDTO = userService.addUser(userRequestDTO);
         return new ServiceResponse<>(HttpStatus.ACCEPTED, userResponseDTO);
+    }
+
+    @GetMapping("/log")
+    public String log() {
+        logger.trace("Trace Level Log");
+        logger.debug("Debug Level Log");
+        logger.info("Info Level Log");
+        logger.warn("Warn Level Log");
+        logger.error("Error Level Log");
+        return "Log printed in console...";
     }
 }

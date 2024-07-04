f7e460df2d1305a42e4c4ea2be042b03cc591585
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-07-05 03:54:20 +0530

    Spring Data Auditing implementation added

--- src/main/java/com/springboot/entity/Product.java
+++ src/main/java/com/springboot/entity/Product.java
@@ -4,12 +4,20 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "PRODUCT_TABLE")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Product {
 
     @Id
@@ -19,4 +27,18 @@ public class Product {
     private double price;
     private String description;
     private String productType;
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
 }

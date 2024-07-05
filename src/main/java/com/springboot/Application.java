db839344555a8886d53ed8dc5707ab57b2932b13
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-07-05 19:25:03 +0530

    Username and Password encryption using Jasypt implemented

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.springboot;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {

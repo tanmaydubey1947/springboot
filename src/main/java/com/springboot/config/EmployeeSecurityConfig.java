fde68ae7d44d2fc0c70855167a1a774e988cce97
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-07-13 00:51:37 +0530

    Spring JWT implementation added

--- src/main/java/com/springboot/config/EmployeeSecurityConfig.java
+++ src/main/java/com/springboot/config/EmployeeSecurityConfig.java
@@ -1,21 +1,32 @@
 package com.springboot.config;
 
 
+import com.springboot.filter.JwtAuthFilter;
 import com.springboot.service.EmployeeUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class EmployeeSecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -42,17 +53,44 @@ public class EmployeeSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http.csrf().disable()
+//                .authorizeHttpRequests()
+//                .antMatchers("/employees/welcome", "/employees/create", "/employees/authenticate").permitAll()
+//                .and()
+//                .authorizeHttpRequests().antMatchers("/employees/**")
+//                .authenticated().and().httpBasic().and().build();
+
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .antMatchers("/employees/welcome", "/employees/create").permitAll()
+                .antMatchers("/employees/welcome", "/employees/create", "/employees/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests().antMatchers("/employees/**")
-                .authenticated().and().httpBasic().and().build();
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 
 }
--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.springboot.controller;
 
 
+import com.springboot.dto.AuthRequest;
 import com.springboot.entity.Employee;
 import com.springboot.service.EmployeeService;
+import com.springboot.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +21,12 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 
     @GetMapping("/welcome")
@@ -48,6 +60,17 @@ public class EmployeeController {
     @PreAuthorize("hasAuthority('ROLE_HR')")
     public Employee updateRole(@RequestBody Employee employee) {
         return employeeService.changeRoleOfEmployee(employee);
+    }
+
+
+    @PostMapping("/authenticate")
+    public String authenticate(@RequestBody AuthRequest authRequest) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authenticate.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("Authentication Failure...");
+        }
     }
 
 }

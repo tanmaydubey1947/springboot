ff84f471c7569b1a46ab1e8cec916271aaa62361
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-07-13 13:54:24 +0530

    OAuth2 Security implementation added

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -1,10 +1,23 @@
 package com.springboot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+
+	@GetMapping("/info")
+	public String getUserInfoFromGitHub(Principal principal) throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(principal);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);

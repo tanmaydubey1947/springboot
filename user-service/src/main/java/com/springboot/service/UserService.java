17ba3e72fcf76f7a8bc23ac2657ff63f60affaac
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-07-18 18:56:43 +0530

    User Service implemented

--- user-service/src/main/java/com/springboot/service/UserService.java
+++ user-service/src/main/java/com/springboot/service/UserService.java
@@ -1,41 +1,28 @@
 package com.springboot.service;
 
-import com.springboot.dao.UserDAO;
-import com.springboot.dto.UserRequestDTO;
-import com.springboot.dto.UserResponseDTO;
-import com.springboot.entity.UserEntity;
-import com.springboot.exception.UserServiceException;
-import com.springboot.utility.UserMapperUtility;
-import jakarta.annotation.PostConstruct;
+import com.springboot.entity.User;
+import com.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile(value = {"dev","stg","prod"})
 public class UserService {
 
     @Autowired
-    UserDAO userDAO;
+    private UserRepository userRepository;
 
-    @Value("${application.message}")
-    private String envMessage;
-
-    @PostConstruct
-    public void init() {
-        System.out.println("******************* " + envMessage);
+    public User addNewUser(User user) {
+        return userRepository.save(user);
     }
 
-    public UserResponseDTO addUser(UserRequestDTO userRequestDTO) {
-        UserEntity userEntity = UserMapperUtility.userDTOtoEntity(userRequestDTO);
-        UserEntity savedEntity = userDAO.save(userEntity);
-        return UserMapperUtility.userEntityToDTO(savedEntity);
+    public User getUser(int userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("user not found with id " + userId));
     }
 
-    public UserResponseDTO getById(int id) {
-        UserEntity userEntity = userDAO.findById(id)
-                .orElseThrow(() -> new UserServiceException(id + " is not a valid course id..."));
-        return UserMapperUtility.userEntityToDTO(userEntity);
+    public User updateAccountStatus(int userId, double usedAmount) {
+        User userDetailsFromDB = getUser(userId);
+        userDetailsFromDB.setAvailableAmount(userDetailsFromDB.getAvailableAmount() - usedAmount);
+        return userRepository.save(userDetailsFromDB);
     }
 }
